11fa4d1946a540ae91aac5717b6a5d9fa9e5f597
rajesh78145 <thakurmahindre@gmail.com>
2026-07-04 15:13:27 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const fs = require("fs");
+
+app.set("view engine", "ejs");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  fs.readdir("./files", (err, files) => {
+    res.render("index", { files: files });
+  });
+});
+app.get("/files/:filename", (req, res) => {
+  fs.readFile(`./files/${req.params.filename}`, "utf-8", (err, filedata) => {
+    res.render("show", {
+      filename: req.params.filename,
+      filedata: filedata,
+    });
+  });
+});
+app.post("/create", (req, res) => {
+  fs.writeFile(
+    `./files/${req.body.title
+      .split(" ")
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join("")
+      .slice(0, 10)}.txt`,
+    req.body.details,
+    (err) => {
+      res.redirect("/");
+    },
+  );
+});
+
+app.listen(3000);
